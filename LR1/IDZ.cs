383282f3cfc7db12391dcd71164c3e6afd881bdb
vladimir <vladimirmawla2@gmail.com>
2022-11-02 12:55:47 +0300

    LR1

---
+++ LR1/IDZ.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    internal class IDZ
+    {
+        public static void DoTask()
+        {
+            Console.Write("Введите количество чисел >");
+            int n = int.Parse(Console.ReadLine());
+            float [] numb = new float[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите "+(i+1)+" число >");
+                numb[i] = float.Parse(Console.ReadLine());
+            }
+            Console.Write("Куб суммы всех чисел равен: ");
+            float sum = numb.Sum();
+            Console.WriteLine(sum*sum*sum);
+        }
+    }
+}
---
+++ LR1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            IDZ.DoTask();
+        }
+    }
+}
